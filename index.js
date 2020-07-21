87ac9a608e0e363c9d48d12f3bbe0ddbc73d8486
Adriano Ferrari <adriano.ferrari@gmail.com>
2020-07-21 10:09:42 -0400

    Serve static files

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.use(express.static("../client/web"));
+
+app.listen(port, () => console.log(`Example app listening at https://localhost:${port}`));
