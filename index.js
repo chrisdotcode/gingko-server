3881314218e31534ad4c6ef5be7031e4e00de83c
Adriano Ferrari <adriano.ferrari@gmail.com>
2020-07-28 19:57:03 +0000

    Create user with server /signup route.

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const config = require("./config.js");
-const nanoSecure = require('nano')(`http://${config.COUCHDB_USER}:${config.COUCHDB_PASS}localhost:5984`)
+const usersDB = require('nano')(`http://${config.COUCHDB_USER}:${config.COUCHDB_PASS}@localhost:5984/_users`);
 const app = express();
 const port = 3000;
 
 
-app.post('/signup', (req, res) => {
+app.use(express.json());
 
+app.post('/signup', async (req, res) => {
+  const dbRes = await usersDB.insert(
+    { type: "user"
+    , roles: []
+    , name: req.body.email
+    , password: req.body.password
+    }, `org.couchdb.user:${req.body.email}`);
+  console.log(dbRes);
+  res.send(dbRes);
 });
 
 app.use(express.static("../client/web"));
