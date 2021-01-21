c2211d6efb783708aa5c8e96c39ee4f5d9fd546a
Adriano Ferrari <adriano.ferrari@gmail.com>
2021-01-21 12:37:53 -0500

    Update config-example.js

--- config-example.js
+++ config-example.js
@@ -1,4 +1,8 @@
 module.exports = {
   COUCHDB_USER: "couchusername",
   COUCHDB_PASS: "couchpassword",
+  SENDGRID_API_KEY: "SG.somekeyvalue",
+  SUPPORT_EMAIL: "support@somedomain.com",
+  STRIPE_SECRET_KEY: "sk_test_123412341234",
+  URL_ROOT: "https://app.example.com",
 };
