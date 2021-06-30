4dc531aea11cc9d729208f04bccf233176351ba8
Adriano Ferrari <adriano.ferrari@gmail.com>
2021-06-30 13:56:18 -0400

    Allow customers to manage their subscriptions (CORS fix)

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true }));
 
 sgMail.setApiKey(config.SENDGRID_API_KEY);
 
